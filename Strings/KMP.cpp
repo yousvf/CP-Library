0bc02fb6450ea78899a36313099ff10d3f6ffb7a
yousvf <145223965+yousvf@users.noreply.github.com>
2026-08-10 23:06:50 +0300

    Add KMP function for pattern matching

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -9,3 +9,19 @@ vector<int> build(string& p) {
 
 	return pi;
 }
+
+vector<int> kmp(string& s, string& p, vector<int>& pi) {
+	vector<int> ret;
+	int n = s.length(), m = p.length();
+	int j = 0;
+	for(int i = 0; i < n; i++) {
+		while(j && s[i] != p[j]) j = pi[j - 1];
+		j += (s[i] == p[j]);
+		if(j == m) {
+			ret.push_back(i - j + 2);
+			j = pi[j - 1];
+		}
+	}
+
+	return ret;
+}
