648259a285cc975dafe60fc44b8c1408c69eb681
yousvf <145223965+yousvf@users.noreply.github.com>
2026-08-10 23:10:18 +0300

    Create Phi Precomputation.cpp

---
+++ Math/Phi Precomputation.cpp
@@ -0,0 +1,11 @@
+int phi[1000001];
+void phi_1_to_n(int n) {
+	phi[0] = 0;
+	phi[1] = 1;
+	for(int i = 2; i <= n; i++)
+		phi[i] = i - 1;
+
+	for(int i = 2; i <= n; i++)
+		for(int j = 2 * i; j <= n; j += i)
+			phi[j] -= phi[i];
+}
